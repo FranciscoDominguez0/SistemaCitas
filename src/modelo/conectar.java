1dd68036f727bedb584d14c3660e1c47f4a0e1a5
Francisco <dominguezf225@gmail.com>
2025-06-08 16:45:46 -0500

    Cambio en conectar

--- src/modelo/conectar.java
+++ src/modelo/conectar.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// esto es mi cambio
 package modelo;
 
 import java.sql.Connection;
